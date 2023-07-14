6e37686020ea7e0ea7f795c7b1603e39aa5759f9
Aldo Meza <aldomezamoreno@gmail.com>
2023-07-14 17:51:42 -0500

    💄 ANTD button added for testing

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,3 +1,11 @@
-export default function Page(){
-    return <section>Cart Page</section>
+'use client'
+import { Button } from 'antd'
+
+export default function Page() {
+  return (
+    <div>
+      <section>Cart Page</section>
+      <Button type='primary'>Button</Button>
+    </div>
+  )
 }
