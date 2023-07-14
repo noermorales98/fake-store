e0c7bcc1ffa6ac0024f7adb5e3469d1b1413d69e
Noeli <noe.rmorales98@gmail.com>
2023-07-14 17:38:38 -0500

    🎉 routes defined

---
+++ src/app/products/[details]/page.js
@@ -0,0 +1,3 @@
+export default function Page({params}){
+    return <section>Product Details Page</section>
+}
---
+++ src/app/products/add/layout.js
@@ -0,0 +1,3 @@
+export default function AddLayout({children}){
+    return <section>{children}</section>
+}
