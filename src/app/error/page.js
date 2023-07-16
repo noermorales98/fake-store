1a5d1b821ce818be98ac151bbb23e348e4b16da2
Noeli <noe.rmorales98@gmail.com>
2023-07-15 20:05:54 -0500

    ✨ add search option

---
+++ src/app/error/page.js
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center h-[30rem]">
+      <h1 className="text-3xl font-semibold text-gray-800 py-2">Error</h1>
+      <h2 className="text-2xl font-semibold text-gray-600 py-2">
+        No se ha encontrado lo que buscas
+      </h2>
+    </div>
+  );
+}
